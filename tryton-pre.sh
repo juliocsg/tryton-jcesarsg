83870b6592be666fe03f3b2d133e6a8d490d7578
Julio César <jcesarsaucedo1993@gmail.com>
2019-03-12 11:38:21 -0300

    Tryton cesar

---
+++ tryton-pre.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get install python
+sudo apt-get install python-pip
+sudo pip install wrapt
+sudo pip install lxml
+sudo pip install relatorio
+sudo pip install genshi
+sudo pip install polib 
+sudo pip install python-sql
+sudo pip install passlib
